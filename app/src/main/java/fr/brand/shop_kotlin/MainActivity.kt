0d5bb2bb0da2b289658acf1d3e84d5f6e85fe70d
sofiane <sofiane.ouldamara@epsi.fr>
2021-12-09 15:20:59 +0100

    ajout boutons zones

--- app/src/main/java/fr/brand/shop_kotlin/MainActivity.kt
+++ app/src/main/java/fr/brand/shop_kotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package fr.brand.shop_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonZone1: Button = findViewById(R.id.buttonZone1)
+        val buttonZone2: Button = findViewById(R.id.buttonZone2)
     }
 }
